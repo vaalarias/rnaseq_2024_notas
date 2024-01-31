17b21da416fdac58aa99cdbfcfad32def3024107
Valentina Arias Ojeda <vjarias@lcg.unam.mx>
2024-01-31 13:54:36 -0600

    app

---
+++ R/app.R
@@ -0,0 +1,29 @@
+## library
+library(recount3)
+library("iSEE")
+
+## Human data projects in recount3
+human_projects <- available_projects()
+## Encuentra tu proyecto de interés. Aquí usaremos
+## SRP009615 de ejemplo
+proj_info <- subset(
+  human_projects,
+  project == "SRP009615" & project_type == "data_sources"
+)
+## Crea un objeto de tipo RangedSummarizedExperiment (RSE)
+## con la información a nivel de genes
+rse_gene_SRP009615 <- create_rse(proj_info)
+## Objeto
+rse_gene_SRP009615
+## Convirtamos las cuentas por nucleotido a cuentas por lectura
+## usando compute_read_counts().
+## Para otras transformaciones como RPKM y TPM, revisa transform_counts().
+assay(rse_gene_SRP009615, "counts") <- compute_read_counts(rse_gene_SRP009615)
+## Para este estudio en específico, hagamos más fácil de usar la
+## información del experimento
+rse_gene_SRP009615 <- expand_sra_attributes(rse_gene_SRP009615)
+colData(rse_gene_SRP009615)[
+  ,
+  grepl("^sra_attribute", colnames(colData(rse_gene_SRP009615)))
+]
+iSEE(rse_gene_SRP009615)
