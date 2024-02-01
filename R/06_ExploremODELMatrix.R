94c0b3b58a9f82ed10e3b4cbe851719e3614e259
Valentina Arias Ojeda <vjarias@lcg.unam.mx>
2024-02-01 13:48:44 -0600

    Initial commit

--- R/06_ExploremODELMatrix.R
+++ R/06_ExploremODELMatrix.R
@@ -25,3 +25,40 @@ app <- ExploreModelMatrix(
   designFormula = ~ genotype + treatment
 )
 if (interactive()) shiny::runApp(app)
+## Ejemplo 2
+(sampleData <- data.frame(
+  Response = rep(c("Resistant", "Sensitive"), c(12, 18)),
+  Patient = factor(rep(c(1:6, 8, 11:18), each = 2)),
+  Treatment = factor(rep(c("pre","post"), 15)),
+  ind.n = factor(rep(c(1:6, 2, 5:12), each = 2))))
+app <- ExploreModelMatrix(
+  sampleData = sampleData,
+  designFormula = ~ Response + Response:ind.n + Response:Treatment
+)
+#> The `name` provided ('') does not correspond to a known icon
+#> The `name` provided ('hand-o-right') does not correspond to a known icon
+#> The `name` provided ('question-circle fa-1g') does not correspond to a known icon
+if (interactive()) shiny::runApp(app)
+## Ejemplo 3
+(sampleData = data.frame(
+  condition = factor(rep(c("ctrl_minus", "ctrl_plus",
+                           "ko_minus", "ko_plus"), 3)),
+  batch = factor(rep(1:6, each = 2))))
+#>     condition batch
+#> 1  ctrl_minus     1
+#> 2   ctrl_plus     1
+#> 3    ko_minus     2
+#> 4     ko_plus     2
+#> 5  ctrl_minus     3
+#> 6   ctrl_plus     3
+#> 7    ko_minus     4
+#> 8     ko_plus     4
+#> 9  ctrl_minus     5
+#> 10  ctrl_plus     5
+#> 11   ko_minus     6
+#> 12    ko_plus     6
+vd <- VisualizeDesign(sampleData = sampleData,
+                      designFormula = ~ 0 + batch + condition,
+                      textSizeFitted = 4, lineWidthFitted = 20,
+                      dropCols = "conditionko_minus")
+cowplot::plot_grid(plotlist = vd$plotlist, ncol = 1)
